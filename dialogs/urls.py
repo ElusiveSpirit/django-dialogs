f59a1de6dcaa92df6b51bcad860ec8b9d3100b73
ElusiveSpirit <2447785@gmail.com>
2016-07-03 19:04:32 +0300

    Tornado chat

--- dialogs/urls.py
+++ dialogs/urls.py
@@ -1,25 +1,10 @@
-"""votesite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
-app_name = 'polls'
+app_name = 'dialogs'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^send_message/$', views.send_message_view),
+    url(r'^send_message_api/(?P<thread_id>\d+)/$', views.send_message_api_view),
+    url(r'^chat/(?P<thread_id>\d+)/$', views.chat_view),
+    url(r'^$', views.messages_view),
 ]
--- django_chat/settings.py
+++ django_chat/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SESSION_ENGINE = 'redis_sessions.session'
+API_KEY = '$0m3-U/\/1qu3-K3Y'
 
+SEND_MESSAGE_API_URL = 'http://127.0.0.1:8000/messages/send_message_api'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'redis_sessions',
     'dialogs',
 ]
 
