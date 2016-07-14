7e9845d6b75f12cb8f635c2e0a6d7db0cc7792e1
ElusiveSpirit <2447785@gmail.com>
2016-07-14 12:23:15 +0300

    Notification classes

--- dialogs/static/dialogs/notifications.js
+++ dialogs/static/dialogs/notifications.js
@@ -5,110 +5,203 @@ Required:
     jquery,
     move
 
+Base div class = notification-container
+
 Shows notification array and organize it's
 */
+class NotificationHandler {
 
-function NotificationHandler() {
-    this._queue = [];
-    // For history..
-    this._storage = [];
-    this._timeout = 3000;
+    constructor() {
+        this._queue = [];
+        // For history..
+        this._storage = [];
+        this._default_timeout = 4000;
 
-    this._container = document.createElement('div');
-    this._container.className = "notification-container";
-    document.body.appendChild(this._container);
-}
+        this._container = document.createElement('div');
+        this._container.className = "notification-container";
+        document.body.appendChild(this._container);
+    }
 
-NotificationHandler.prototype.addNotification = function(notification) {
-    this._queue.push(notification);
-    var index = setTimeout(this.removeNotification, this._timeout, this, notification);
-    this._storage.push({
-        'id' : index,
-        'notification' : notification,
-        'time' : Date.now(),
-        'has_read' : false,
-    });
-    notification.setIndex(index);
-    this.addObj(notification.getObj());
-    notification.show();
-}
+    addNotification(notification) {
+        this._queue.push(notification);
 
-NotificationHandler.prototype.removeNotification = function(handler, notification) {
-    handler._queue.forEach(function(item, i, arr) {
-        if (item.getIndex() == notification.getIndex()) {
-            var removing_obj = item;
-            delete handler._queue[i];
-            removing_obj.hide();
-            return false;
+        let timeout = notification.timeout;
+        if (typeof timeout == 'undefined') {
+            timeout = this._default_timeout;
         }
-    });
-}
 
-NotificationHandler.prototype.addObj = function(obj) {
-    this._container.appendChild(obj);
-}
+        var index = setTimeout(
+            this.removeNotification.bind(this)
+        , timeout, notification);
 
+        this._storage.push({
+            'id' : index,
+            'notification' : notification,
+            'time' : Date.now(),
+            'has_read' : false,
+        });
+        notification.index = index;
+        this.addObj(notification.obj);
+        notification.show();
+    }
 
-function Notification(text) {
-    this._text = text;
-    this._moving_length = 50;
-    this._template_top = '';
-    this._template_bot = '';
-
-    this._obj = document.createElement('div');
-    this._obj.className = "notification";
-    this._obj.setAttribute("style", "opacity: 0.0;");
-    this._obj.innerHTML = this._template_top + this._text + this._template_bot;
-}
-
-Notification.prototype.getObj = function() {
-    return this._obj;
-}
-
-Notification.prototype.setIndex = function(index) {
-    this._index = index;
-    this._obj.id = "notification_" + index;
-}
-
-Notification.prototype.getIndex = function() {
-    return this._index;
-}
-
-Notification.prototype.getID = function() {
-    return "notification_" + this.getIndex();
-}
-
-Notification.prototype.getHeight = function() {
-    return $('#notification_' + this._index).outerHeight(true);
-}
-
-Notification.prototype.show = function() {
-    var obj = document.getElementById(this.getID());
-    function appear() {
-        var opacity = parseFloat(window.getComputedStyle(obj).getPropertyValue("opacity"));
-        if (opacity < 1) {
-            obj.style.opacity = opacity + 0.1;
-            setTimeout(appear, 20);
+    removeNotification(notification) {
+        var that = this;
+        clearTimeout(notification.index);
+        if (notification.autoremove) {
+            that._queue.forEach(function(item, i, arr) {
+                if (item.index == notification.index) {
+                    var removing_obj = item;
+                    delete that._queue[i];
+                    removing_obj.hide();
+                    return false;
+                }
+            });
         }
     }
-    setTimeout(appear, 20);
+
+    addObj(obj) {
+        this._container.appendChild(obj);
+    }
 }
 
-Notification.prototype.hide = function() {
-    var obj = document.getElementById(this.getID());
-    var height = this.getHeight();
-    function dissaper() {
-        var opacity = parseFloat(window.getComputedStyle(obj).getPropertyValue("opacity"));
-        if (opacity > 0) {
-            obj.style.opacity = opacity - 0.1;
-            setTimeout(dissaper, 20);
-        } else {
-            move(obj)
-              .sub('margin-top', height)
-              .end(function (){
-                obj.remove();
-              });
+
+class BaseNotification {
+
+    constructor(html, timeout=1000, autoremove=true) {
+        this._timeout = timeout;
+        this._autoremove = autoremove;
+
+        this._obj = document.createElement('div');
+        this._obj.className = "notification";
+        this._obj.setAttribute("style", "opacity: 0.0;");
+        this._obj.innerHTML = html;
+    }
+
+    set extra_classes(classes) {
+        this._obj.className += ` ${classes}`;
+    }
+
+    set index(index) {
+        this._index = index;
+        this._obj.id = `notification_${index}`;
+    }
+
+    get index() {
+        return this._index;
+    }
+
+    get obj_id() {
+        return `notification_${this.index}`;
+    }
+
+    get autoremove() {
+        return this._autoremove;
+    }
+
+    get timeout() {
+        return this._timeout;
+    }
+
+    get element() {
+        return document.getElementById(this.obj_id);
+    }
+
+    get obj() {
+        return this._obj;
+    }
+
+    get obj_height() {
+        return $(`#${this.obj_id}`).outerHeight(true);
+    }
+
+    get innerHTML() {
+        return this._obj.innerHTML;
+    }
+
+    set click(func) {
+        this._obj.onclick = func;
+    }
+
+    show() {
+        var obj = this.element;
+        function appear() {
+            var opacity = parseFloat(window.getComputedStyle(obj).getPropertyValue("opacity"));
+            if (opacity < 1) {
+                obj.style.opacity = opacity + 0.1;
+                setTimeout(appear, 20);
+            }
+        }
+        setTimeout(appear, 20);
+    }
+
+    hide() {
+        var obj = this.element;
+        var height = this.obj_height;
+        function dissaper() {
+            var opacity = parseFloat(window.getComputedStyle(obj).getPropertyValue("opacity"));
+            if (opacity > 0) {
+                obj.style.opacity = opacity - 0.1;
+                setTimeout(dissaper, 20);
+            } else {
+                move(obj)
+                  .sub('margin-top', height)
+                  .end(function (){
+                    obj.remove();
+                  });
+            }
+        }
+        setTimeout(dissaper, 20);
+    }
+}
+
+class AlertNotification extends BaseNotification {
+
+    constructor(text, timeout=3000) {
+        super(text, timeout);
+        this.extra_classes = 'alert';
+    }
+}
+
+class ErrorNotification extends BaseNotification {
+
+    constructor(text, timeout=3000) {
+        super(text, timeout);
+        this.extra_classes = 'error';
+    }
+}
+
+class InfoNotification extends BaseNotification {
+
+    constructor(text) {
+        super(text, undefined, false);
+        this.extra_classes = 'info';
+        var that = this;
+        this.click = function (){
+            that.hide();
         }
     }
-    setTimeout(dissaper, 20);
+}
+
+class MessageNotification extends BaseNotification {
+
+    constructor(author, img, text, date) {
+        var template = `
+            <div class="message">
+              <img src="${img}" width="40" height="40" />
+              <div class="content">
+                <h4>${author}</h4>
+                <p>${text}</p>
+                <p class="date">${date}</p>
+              </div>
+              <div style="clear:both;"></div>
+            </div>`;
+
+        super(template, undefined, false);
+
+        var that = this;
+        this.click = function (){
+            that.hide();
+        }
+    }
 }
